6ea0c397a67b0e4f9604b925ae4d48b47d48ce5a
Jason Sommer <jsdir@users.noreply.github.com>
2014-05-03 05:51:10 -0500

    Added tests for cassandra datastore

--- test/datastores_test.js
+++ test/datastores_test.js
@@ -78,32 +78,74 @@ describe('RedisDatastore', function() {
   });
 });
 
-xdescribe('CassandraDatastore', function() {
+describe('CassandraDatastore', function() {
 
-  before(function() {
+  before(function(done) {
+    // Add a testing table to the database.
+    var query = 'CREATE TABLE family (primary_key text PRIMARY KEY, ' +
+      'string text, integer int, "booleanTrue" boolean, "booleanFalse" boolean, ' +
+      'datetime timestamp, date int);';
+    cassandraClient.connect(function(err) {
+      if (err) {
+        throw err;
+      } else {
+        cassandraClient.cql(query, done)
+      }
+    })
+
     this.cds = new datastores.CassandraDatastore({
       cassandra: cassandraClient
-    }, {})
+    }, {
+      attributes: {
+        primary_key: {type: 'string'},
+        integer: {type: 'integer'},
+        string: {type: 'string'},
+        booleanTrue: {type: 'boolean'},
+        booleanFalse: {type: 'boolean'},
+        datetime: {type: 'datetime'},
+        date: {type: 'date'}
+      },
+      table: 'family',
+      pkAttribute: 'primary_key'
+    });
+  });
+
+  after(function(done) {
+    // Remove the testing table from the database.
+    var query = 'DROP TABLE family;';
+    cassandraClient.connect(function(err) {
+      if (err) {
+        throw err;
+      } else {
+        cassandraClient.cql(query, done)
+      }
+    });
   });
 
   it('persists to cassandra', function(done) {
     var cds = this.cds;
 
-    // Fetching with a primary key value that does not exist.
-    //rds.fetch('modelType', 'id', 1234)
+    var datetime = new Date(2010, 1, 2, 3, 4, 5, 6);
+    var date = new Date(2010, 1, 1);
+    var attributes = ['integer', 'string', 'booleanTrue', 'booleanFalse',
+      'datetime', 'date'];
 
-    cds.save('models', null, {
-      id: 1234,
-      foo: 'string1',
-      foo2: 'string2',
-      bar: 1,
-      bar2: 2//new Date()
-    }, function(err, result) {
-      cds.fetch('models', 'id', 1234,
-        ['foo2', 'bar', 'bar2']
-      , function(err, result) {
-        console.log('w');
-        console.log(result[0].get('bar').value);
+    cds.save({
+      primary_key: 'key',
+      integer: 1234,
+      string: 'string',
+      booleanTrue: true,
+      booleanFalse: false,
+      datetime: datetime,
+      date: date
+    }, function(err) {
+      cds.fetch('key', attributes, function(err, result) {
+        result.integer.should.equal(1234);
+        result.string.should.equal('string');
+        result.booleanTrue.should.be.true
+        result.booleanFalse.should.be.false
+        result.datetime.getTime().should.equal(datetime.getTime());
+        result.date.getTime().should.equal(date.getTime());
         done();
       });
     });
