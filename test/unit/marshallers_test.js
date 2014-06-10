54ceb276d020b78af244d8f168b159a7363bd835
Jason Sommer <jsdir@users.noreply.github.com>
2014-06-10 16:12:08 -0500

    Change to geolocation

--- test/unit/marshallers_test.js
+++ test/unit/marshallers_test.js
@@ -3,28 +3,28 @@ var marshallers = require('../../lib/marshallers.js');
 describe('BasicMarshaller', function() {
 
   it('should serialize geotags', function() {
-    marshallers.BasicMarshaller.serializers.geotag({
+    marshallers.BasicMarshaller.serializers.geolocation({
       lat: 21.5, lon: 50.2, foo: 'bar'
     }).should.deep.equal([null, '{"lat":21.5,"lon":50.2}']);
   });
 
   it('should unserialize geotags', function() {
-    marshallers.BasicMarshaller.unserializers.geotag(
+    marshallers.BasicMarshaller.unserializers.geolocation(
       '{"lat": 21.5, "lon": 50.2}'
     ).should.deep.equal([null, {lat: 21.5, lon: 50.2}]);
   });
 
   it('should validate when serializing geotags', function() {
-    marshallers.BasicMarshaller.serializers.geotag({
+    marshallers.BasicMarshaller.serializers.geolocation({
       lat: 21.5
-    }).should.deep.equal(['invalid geotag']);
+    }).should.deep.equal(['invalid geolocation']);
 
-    marshallers.BasicMarshaller.serializers.geotag({
+    marshallers.BasicMarshaller.serializers.geolocation({
       lat: 21.5, lon: -50.2
-    }).should.deep.equal(['invalid geotag']);
+    }).should.deep.equal(['invalid geolocation']);
 
-    marshallers.BasicMarshaller.serializers.geotag({
+    marshallers.BasicMarshaller.serializers.geolocation({
       lat: 21.5, lon: 100.2
-    }).should.deep.equal(['invalid geotag']);
+    }).should.deep.equal(['invalid geolocation']);
   });
 });
