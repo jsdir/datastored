bc815d441ba185f66fbedf5c7c31418b60cd32cc
Jason Sommer <jsdir@users.noreply.github.com>
2014-09-23 10:43:36 -0500

    Added loadTree redis script

--- lib/datastores/scripts/redis.lua
+++ lib/datastores/scripts/redis.lua
@@ -1,28 +1,29 @@
---[[
 local rootKey = KEYS[1]
-
-local includeLeaves = KEYS[1]
-local nodePrefix = KEYS[2]
-local nodeSuffix = KEYS[3]
-local attributes = ARGS
-local res
+local childKeyPrefix = KEYS[2]
+local childRelation = KEYS[3]
+local includeLeaves = KEYS[4]
+local maxLevels = KEYS[5]
 
 function loadTree (key,level)
-  -- local sum = 0
-  data = {}
+  local data = {}
+  local res = redis.call('lrange',key,0,-1)
 
-  res = redis.call('lrange', key, 0, -1)
   for id in res do
-    -- save
-    data[id]
-    if key < maxLevels
-      newKey = childPrefix .. id
-      loadTree(newKey, key + 1)
+    local instance = {id=id}
+
+    -- Fetch instance attributes if requested.
+    if next(ARGS) != nil then
+      instance.data = redis.call('hgetall',key,ARGS)
     end
+
+    if level == 0 or level < maxLevels
+      childKey = childKeyPrefix .. id
+      instance.children = loadTree(childKey,level + 1)
+    end
+    -- TODO: strip leaves if requested
   end
 
   return data
 end
 
 return loadTree(rootKey,1)
-]]--
