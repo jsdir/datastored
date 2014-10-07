048873c9dc496a3b193bc466b2df206dad299704
Jason Sommer <jsdir@users.noreply.github.com>
2014-10-07 11:36:09 -0500

    Added id serialization to datastores

--- test/integration/datastores_test.js
+++ test/integration/datastores_test.js
@@ -26,17 +26,14 @@ var datastores = {
   MemoryDatastore: new MemoryDatastore()
 };
 
+//delete datastores.CassandraDatastore;
+//delete datastores.RedisDatastore;
+//delete datastores.MemoryDatastore;
+
 _.each(datastores, function(datastore, name) {
   describe(name, function() {
 
     var isCassandra = (name === 'CassandraDatastore');
-
-    beforeEach(function(cb) {
-      datastore.reset(function(err) {
-        if (err) {return cb(err);}
-        cb();
-      });
-    });
 
     var baseTypes = {
       bar: 'integer',
@@ -44,13 +41,15 @@ _.each(datastores, function(datastore, name) {
       booleanTrue: 'boolean',
       booleanFalse: 'boolean',
       datetime: 'datetime',
-      date: 'date'
+      date: 'date',
+      id: 'string'
     };
 
     var datetime = 1264982400000;
     var date = 1264982400000;
 
     var baseOptions = {
+      idName: 'id',
       table: 'test_table',
       indexes: [],
       replaceIndexes: [],
@@ -65,10 +64,12 @@ _.each(datastores, function(datastore, name) {
       types: baseTypes
     };
 
-    function assertFind(table, index, value, id, cb) {
-      datastore.find({
-        table: table, index: index, value: value
-      }, function(err, res) {
+    beforeEach(function(cb) {
+      datastore.reset(cb);
+    });
+
+    function assertFind(options, id, cb) {
+      datastore.find(options, function(err, res) {
         if (err) {return cb(err);}
         expect(res).to.eq(id);
         cb();
@@ -83,61 +84,89 @@ _.each(datastores, function(datastore, name) {
       datastore.save(options, cb);
     }
 
-    function assertNotFound(id, cb) {
-      datastore.fetch({
-        table: 'test_table', ids: [id], attributes: ['bar'], types: baseTypes
-      }, function(err, data) {
-        if (err) {return cb(err);}
-        var expectedData = {};
-        expectedData[id] = null;
-        data.should.deep.eq(expectedData);
-        cb();
-      });
-    }
-
     describe('#save()', function() {
 
       it('should save a row with an id of type string', function(done) {
-        var options = _.merge({}, baseOptions, {id: 'foo'});
-        datastore.save(options, function(err) {
-          if (err) {return done(err);}
-          datastore.fetch({
-            table: 'test_table',
-            ids: ['foo'],
-            attributes: [
-              'bar',
-              'baz',
-              'booleanTrue',
-              'booleanFalse',
-              'datetime',
-              'date'
-            ],
-            types: baseTypes
-          }, function(err, data) {
-            if (err) {return done(err);}
-            data['foo'].bar.should.eq(123);
-            data['foo'].baz.should.eq('foobar');
-            data['foo'].booleanTrue.should.be.true;
-            data['foo'].booleanFalse.should.be.false;
-            data['foo'].datetime.getTime().should.equal(datetime);
-            data['foo'].date.getTime().should.equal(date);
-            done();
-          });
-        });
+        async.parallel([
+          function(cb) {
+            // Save with string id.
+            var options = _.merge({}, baseOptions, {id: 'foo'});
+            datastore.save(options, function(err) {
+              if (err) {return cb(err);}
+              datastore.fetch({
+                table: 'test_table',
+                id: 'foo',
+                idName: 'id',
+                attributes: [
+                  'bar',
+                  'baz',
+                  'booleanTrue',
+                  'booleanFalse',
+                  'datetime',
+                  'date'
+                ],
+                types: baseTypes
+              }, function(err, data) {
+                if (err) {return cb(err);}
+                data.bar.should.eq(123);
+                data.baz.should.eq('foobar');
+                data.booleanTrue.should.be.true;
+                data.booleanFalse.should.be.false;
+                data.datetime.getTime().should.equal(datetime);
+                data.date.getTime().should.equal(date);
+                cb();
+              });
+            });
+          },
+          function(cb) {
+            // Save with string id.
+            var options = _.merge({}, baseOptions, {id: 'foos'});
+            datastore.save(options, function(err) {
+              if (err) {return cb(err);}
+              datastore.fetch({
+                table: 'test_table',
+                id: 'foos',
+                idName: 'id',
+                attributes: [
+                  'bar',
+                  'baz',
+                  'booleanTrue',
+                  'booleanFalse',
+                  'datetime',
+                  'date'
+                ],
+                types: baseTypes
+              }, function(err, data) {
+                if (err) {return cb(err);}
+                data.bar.should.eq(123);
+                data.baz.should.eq('foobar');
+                data.booleanTrue.should.be.true;
+                data.booleanFalse.should.be.false;
+                data.datetime.getTime().should.equal(datetime);
+                data.date.getTime().should.equal(date);
+                cb();
+              });
+            });
+          }
+        ], done);
       });
 
       it('should save a row with an id of type integer', function(done) {
-        var options = _.merge({}, baseOptions, {id: 2, table: 'test_table_int'});
+        // Save with integer id.
+        var options = _.merge({}, baseOptions, {
+          id: 2, table: 'test_table_int', types: {id: 'integer'}
+        });
         datastore.save(options, function(err) {
           if (err) {return done(err);}
           datastore.fetch({
             table: 'test_table_int',
-            ids: [2],
+            id: 2,
+            idName: 'id',
             attributes: ['bar', 'baz'],
-            types: baseTypes
+            types: _.extend({}, baseTypes, {id: 'integer'})
           }, function(err, data) {
             if (err) {return done(err);}
-            data.should.deep.eq({2: {bar: 123, baz: 'foobar'}});
+            data.should.deep.eq({bar: 123, baz: 'foobar'});
             done();
           });
         });
@@ -158,13 +187,13 @@ _.each(datastores, function(datastore, name) {
           function(cb) {
             datastore.fetch({
               table: 'test_table',
-              ids: ['foo'],
+              id: 'foo',
+              idName: 'id',
               attributes: ['bar', 'baz'],
               types: baseTypes
             }, function(err, data) {
               if (err) {return cb(err);}
-              data['foo'].bar.should.eq(123);
-              data['foo'].baz.should.eq('foo');
+              data.should.deep.eq({bar: 123, baz: 'foo'});
               cb();
             });
           }
@@ -187,13 +216,14 @@ _.each(datastores, function(datastore, name) {
           function(cb) {
             datastore.fetch({
               table: 'test_table',
-              ids: ['foo'],
+              id: 'foo',
+              idName: 'id',
               attributes: ['bar', 'baz'],
               types: baseTypes
             }, function(err, data) {
               if (err) {return cb(err);}
-              data['foo'].bar.should.eq(456);
-              expect(data['foo'].baz).to.be.undefined;
+              data.bar.should.eq(456);
+              expect(data.baz).to.be.undefined;
               cb();
             });
           }
@@ -203,10 +233,19 @@ _.each(datastores, function(datastore, name) {
       // CassandraDatastore should not implement these yet.
       if (!isCassandra) {
 
+        function assertHasIndex(value, id, cb) {
+          assertFind({
+            table: 'test_table',
+            index: 'bar',
+            value: value,
+            types: {bar: 'integer'}
+          }, id, cb);
+        }
+
         it('should increment tables within a row', function(done) {
           var options = {
-            id: 'foo', table: 'count',
-            types: {i1: 'integer', i2: 'integer', i3: 'integer'}
+            id: 'foo', idName: 'id', table: 'count',
+            types: {i1: 'integer', i2: 'integer', i3: 'integer', id: 'string'}
           };
 
           var saveOptions = _.extend({}, options, {data: {i1: 1, i2: 2, i3: 3}});
@@ -218,10 +257,10 @@ _.each(datastores, function(datastore, name) {
             datastore.save(saveOptions, function(err) {
               if (err) {return done(err);}
               var saveOptions = _.extend({}, options, {
-                ids: ['foo'], attributes: ['i1', 'i2', 'i3']
+                id: 'foo', attributes: ['i1', 'i2', 'i3']
               });
               datastore.fetch(saveOptions, function(err, data) {
-                data['foo'].should.deep.eq({i1: 0, i2: 2, i3: 4});
+                data.should.deep.eq({i1: 0, i2: 2, i3: 4});
                 done();
               });
             });
@@ -230,8 +269,8 @@ _.each(datastores, function(datastore, name) {
 
         it('should save indexes', function(done) {
           saveIndexedModel(123, {}, function(err) {
-            if (err) {done(err);}
-            assertFind('test_table', 'bar', 123, 'foo', done);
+            if (err) {return done(err);}
+            assertHasIndex(123, 'foo', done);
           });
         });
 
@@ -252,8 +291,8 @@ _.each(datastores, function(datastore, name) {
           ], function(err) {
             if (err) {done(err);}
             async.parallel([
-              function(cb) {assertFind('test_table', 'bar', 456, 'foo', cb);},
-              function(cb) {assertFind('test_table', 'bar', 123, 'foo', cb);}
+              function(cb) {assertHasIndex(123, 'foo', cb);},
+              function(cb) {assertHasIndex(456, 'foo', cb);}
             ], done);
           });
         });
@@ -265,13 +304,23 @@ _.each(datastores, function(datastore, name) {
           ], function(err) {
             if (err) {done(err);}
             async.parallel([
-              function(cb) {assertFind('test_table', 'bar', 456, 'foo', cb);},
-              function(cb) {assertFind('test_table', 'bar', 123, null, cb);}
+              function(cb) {assertHasIndex(456, 'foo', cb);},
+              function(cb) {assertHasIndex(123, null, cb);}
             ], done);
           });
         });
       }
     });
+
+    function fetchModel(cb, id) {
+      datastore.fetch({
+        table: 'test_table',
+        id: id || 'foo',
+        idName: 'id',
+        attributes: ['bar'],
+        types: baseTypes
+      }, cb);
+    }
 
     describe('#fetch()', function() {
 
@@ -281,22 +330,19 @@ _.each(datastores, function(datastore, name) {
       });
 
       it('should only fetch the requested attributes', function(done) {
-        datastore.fetch({
-          table: 'test_table',
-          ids: ['foo'],
-          attributes: ['bar'],
-          types: baseTypes
-        }, function(err, data) {
-          if (err) {return cb(err);}
-          data.should.deep.eq({
-            foo: {bar: 123}
-          });
+        fetchModel(function(err, data) {
+          if (err) {return done(err);}
+          data.should.deep.eq({bar: 123});
           done();
         });
       });
 
       it('should callback with null if no row was found', function(done) {
-        assertNotFound('bar', done);
+        fetchModel(function(err, data) {
+          if (err) {return done(err);}
+          expect(data).to.not.exist;
+          done();
+        }, 'undefined');
       });
     });
 
@@ -310,12 +356,19 @@ _.each(datastores, function(datastore, name) {
       });
 
       it('should destroy the row', function(done) {
-        datastore.destroy({table: 'test_table', ids: ['foo']}, function(err) {
+        datastore.destroy({
+          table: 'test_table', id: 'foo', idName: 'id', types: baseTypes
+        }, function(err) {
           if (err) {return done(err);}
-          assertNotFound('foo', done);
+          fetchModel(function(err, data) {
+            if (err) {return done(err);}
+            expect(data).to.not.exist;
+            done();
+          });
         });
       });
 
+      /*
       if (!isCassandra) {
 
         it('should destroy all indexes', function(done) {
@@ -324,16 +377,17 @@ _.each(datastores, function(datastore, name) {
             function(cb) {saveIndexedModel(456, {bar: 124}, cb);},
             function(cb) {
               var options = _.merge({}, baseOptions, {
-                id: 'foo', indexes: ['bar', 'baz'], data: {baz: 'baz'}
+                id: 'foo', idName: 'id', indexes: ['bar', 'baz'], data: {baz: 'baz'}
               });
               datastore.save(options, cb);
             }
           ], function(err) {
+            if (err) {return done(err);}
             datastore.destroy({
-              table: 'test_table', ids: ['foo'], indexValues: {
+              table: 'test_table', id: 'foo', idName: 'id', indexValues: {
                 values: {bar: [456], baz: ['baz']}, // current values from orm
                 replaceIndexes: ['bar']
-              }
+              }, types: baseTypes
             }, function(err) {
               if (err) {return done(err);}
               // Ensure indexes are deleted.
@@ -345,16 +399,89 @@ _.each(datastores, function(datastore, name) {
             });
           });
         });
-      }
+      }*/
     });
 
     if (!isCassandra) { // Temporary.
-      xdescribe('#addToCollection()', function(done) {
 
+      xdescribe('#addToCollection()', function() {
+
+        it('should add instances to a list', function(done) {
+          async.series([
+            function addToEmptyList(cb) {
+              // Add multiple ids to an empty list.
+              datastore.addToCollection({
+                table: 'test_table',
+                id: 'foo',
+                idName: 'id',
+                types: {id: 'string'},
+                relationName: 'children',
+                instances: [{id: 1, idType: 'integer'}, {id: 2, idType: 'integer'}]
+              }, cb);
+            },
+            function addToExistingList(cb) {
+              // Add multiple ids to an existing list.
+              datastore.addToCollection({
+                table: 'test_table',
+                id: 'foo',
+                idName: 'id',
+                types: {id: 'string'},
+                relationName: 'children',
+                instances: [{id: 3, idType: 'integer'}, {id: 4, idType: 'integer'}]
+              }, cb);
+            },
+            function checkIds(cb) {
+              // Check that ids were stored.
+              datastore.fetchCollection({
+                table: 'test_table',
+                id: 'foo',
+                idName: 'id',
+                types: {id: 'string'},
+                relationName: 'children',
+                childIdType: 'integer'
+                /*,
+                childIdType xor idTypes: {
+                  0: 'string',
+                  1: 'string'
+                }*/
+              }, function(err, data) {
+                if (err) {return cb(err);}
+                data.should.deep.eq([{id: 1}, {id: 2}, {id: 3}, {id: 4}]);
+                cb();
+              });
+            }
+          ], done);
+        });
+
+        xit('should add typed instances to a list', function(done) {
+
+        });
       });
 
       xdescribe('#fetchCollection()', function() {
 
+        beforeEach(function(done) {
+          saveCollection('key', [1, 2, 3, 4], done)
+        });
+
+        it('should fetch a nonexistent collection', function(done) {
+          assertFetchEquals('undefined', [], done);
+        });
+
+        it('should fetch an empty collection', function(done) {
+          saveCollection('key', [1, 2, 3, 4], function(err) {
+            if (err) {return done(err);}
+            assertFetchEquals('key', [], done);
+          });
+        });
+
+        it('should limit returned results', function(done) {
+          assertFetchEquals('key', {limit: 2}, [1, 2], done);
+        });
+
+        it('should offset returned results', function() {
+          assertFetchEquals('key', {limit: 2, offset: 1}, [2, 3], done);
+        });
       });
 
       xdescribe('#fetchTree', function() {
