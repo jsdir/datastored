3321f83cec81d8544e6d628bd9dd01e2ddb62bd6
Jason Sommer <jsdir@users.noreply.github.com>
2014-11-14 03:16:07 -0600

    Refactored model unit tests

--- test/unit/model_test.js
+++ test/unit/model_test.js
@@ -1,102 +1,91 @@
 var _ = require('lodash');
 var chai = require('chai');
-var sinon = require('sinon');
-var sinonChai = require('sinon-chai');
 
 var datastored = require('../..')
 var testUtils = require('../test_utils');
 
 chai.should();
-chai.use(sinonChai);
+var expect = chai.expect;
 
 describe('Model', function() {
 
   before(function() {
-    var self = this;
-    testUtils.setupOrm.call(this);
-    testUtils.setupTestModels.call(this);
-
-    this.assertCreateFails = function(options, message, newModel) {
-      (function() {
-        if (newModel) {
-          self.createNewModel(options);
-        } else {
-          self.createModel(options);
-        }
-      }).should.throw(message);
-    };
+    testUtils.createTestEnv(this);
+    this.Model = this.models.BasicUnitModel;
+    this.modelOptions = this.options.BasicUnitModel;
   });
 
-  // Test model options.
+  describe('options', function() {
 
-  it('should require attributes', function() {
-    var options = _.omit(testUtils.baseOptions, 'attributes');
-    this.assertCreateFails(options, '"attributes" is not defined', true);
-  });
-
-  it('should require an id type', function() {
-    var options = _.omit(testUtils.baseOptions, 'idType');
-    this.assertCreateFails(options, '"idType" is not defined', true);
-  });
-
-  it('should require a keyspace', function() {
-    var options = _.omit(testUtils.baseOptions, 'keyspace');
-    this.assertCreateFails(options, '"keyspace" is not defined', true);
-  });
-
-  it('should have "staticMethods" from options', function() {
-    this.BasicModel.func().should.deep.eq(this.BasicModel);
-  });
-
-  it('should require the id to be an integer', function() {
-    this.assertCreateFails({
-      idType: 'boolean'
-    }, 'id type can only be "string" or "integer"');
-  });
-
-  describe('#create()', function() {
-
-    it('should transform data by default', function() {
-      var instance = this.BasicModel.create({foo: 'bar'});
-      instance.get('foo', true).should.eq('input(bar)');
+    it('should require attributes', function() {
+      var options = _.omit(this.modelOptions, 'attributes');
+      this.assertCreateFails(options, '"attributes" is not defined');
     });
 
-    it('should not transform data if requested', function() {
-      var instance = this.BasicModel.create({foo: 'bar'}, true);
-      instance.get('foo', true).should.eq('bar');
+    it('should require an id attribute', function() {
+      var options = _.omit(this.modelOptions, 'id');
+      this.assertCreateFails(options, '"id" is not defined');
+    });
+
+    it('should require a keyspace', function() {
+      var options = _.omit(this.modelOptions, 'keyspace');
+      this.assertCreateFails(options, '"keyspace" is not defined');
+    });
+
+    it('should assign "statics"', function() {
+      this.Model.property.should.eq('text');
+      this.Model.func().should.deep.eq(this.Model);
     });
   });
 
-  describe('#get()', function() {
+  describe.only('#build()', function() {
 
-    it('should transform the id by default', function() {
-      var instance = this.BasicModel.get('idValue');
-      instance.get('id', true).should.eq('input(idValue)');
+    it('should build an instance', function() {
+      var instance = this.Model.build({guarded: 'a', text: 'b'});
+      instance.isNew().should.be.true;
+      instance.get('guarded').should.eq('a');
+      instance.get('text').should.eq('b');
     });
 
-    it('should not transform the id if requested', function() {
-      var instance = this.BasicModel.get('idValue', true);
-      instance.get('id', true).should.eq('idValue');
+    it('should transform data if in user mode', function() {
+      var instance = this.Model.userMode().build({
+        guarded: 'a', text: 'b'
+      }).userMode(false);
+      expect(instance.get('guarded')).to.be.undefined
+      instance.get('text').should.eq('unser(input(b))');
+    });
+  });
+
+  describe('#withId()', function() {
+
+    it('should return a model with the given id', function() {
+      this.Model.withId('a').getId().should.eq('a');
     });
 
-    it('should set instance status', function() {
-      var instance = this.BasicModel.get('idValue', false);
-      instance.isNew.should.be.false;
-      instance.isChanged().should.be.false;
+    it('should transform the id value if in user mode', function() {
+      var instance = this.Model.userMode().withId('a');
+      instance.userMode(false).getId().should.eq('unser(input(a))');
+    });
+
+    it('should correctly set instance status', function() {
+      this.Model.withId('a').isNew().should.be.false;
     });
   });
 
   describe('#find()', function() {
 
     it('should ensure that the query attribute is indexed', function() {
-      var BasicModel = this.BasicModel;
+      var Model = this.Model;
 
       // Test undefined attribute.
-      (function() {BasicModel.find('undefined', 'bar', testUtils.noop);})
-        .should.throw('"undefined" is not defined');
+      (function() {
+        Model.find('undefined', 'a');
+      }).should.throw('"undefined" is not defined');
+
       // Test attribute that is not an index.
-      (function() {BasicModel.find('foo', 'bar', testUtils.noop);})
-        .should.throw('attribute "foo" is not an index');
+      (function() {
+        Model.find('text', 'a');
+      }).should.throw('attribute "text" is not an index');
     });
   });
 });
