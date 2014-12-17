beb114811cd824d10a640deb86533fc44fe2341e
Jason Sommer <jsdir@users.noreply.github.com>
2014-12-17 17:59:27 -0600

    Added tests for instance.fetch

--- test/unit/instance_test.js
+++ test/unit/instance_test.js
@@ -110,21 +110,22 @@ describe('Instance', function() {
     });
   });
 
-  xdescribe('#fetch()', function() {
+  describe('#fetch()', function() {
 
-    it('should only fetch requested attributes', function(done) {
-      var scope = ['string'];
-      testUtils.saveAndReload(this.instance, scope, function(err, instance) {
-        if (err) {return done(err);}
-        instance.get('string', true).should.eq('foo');
-        expect(instance.get('integer')).to.be.undefined;
-        done();
+    it('should only fetch requested attributes', function() {
+      var Model = this.Model;
+      return Model.create({text: 'a', text2: 'b'}).then(function(instance) {
+        var newInstance = Model.withId(instance.id);
+        return newInstance.fetch(['text']);
+      }).then(function(instance) {
+        instance.get('text').should.eq('output(a)');
+        expect(instance.get('text2')).to.be.undefined;
       });
     });
 
-    it('should resolve indicating if the instance was found', function(done) {
+    it('should resolve indicating if the instance was found', function() {
       var instance = this.Model.withId('undefined');
-      return instance.fetch('all').then(function(instance) {
+      return instance.fetch('text').then(function(instance) {
         expect(instance).to.be.null;
       });
     });
@@ -133,7 +134,9 @@ describe('Instance', function() {
   xdescribe('#save()', function() {
 
     before(function() {
-      this.RequiredModel = this.createModel({
+      this.RequiredModel = this.orm.createModel('RequiredModel', {
+        keyspace: 'RequiredModel',
+        id: datastored.Id({type: 'string'}),
         attributes: {
           required: datastored.String({
             required: true, hashStores: [true]
@@ -141,7 +144,9 @@ describe('Instance', function() {
         }
       });
 
-      this.ValidationModel = this.createModel({
+      this.ValidationModel = this.orm.createModel('ValidationModel', {
+        keyspace: 'ValidationModel',
+        id: datastored.Id({type: 'string'}),
         attributes: {
           bar: datastored.String({
             hashStores: [true],
@@ -154,7 +159,10 @@ describe('Instance', function() {
         }
       });
 
-      this.ErrorModel = this.createModel({
+      this.ErrorModel = this.orm.createModel('ErrorModel', {
+        keyspace: 'ErrorModel',
+        id: datastored.Id({type: 'string'}),
+        attributes: {text: datastored.String({hashStores: [true]})},
         asyncTransform: {
           save: function(options, data, cb) {
             cb('message');
@@ -205,112 +213,3 @@ describe('Instance', function() {
     });
   });
 });
-
-/*
-var chai = require('chai');
-
-var attributes = require('../../lib/attributes');
-var memoryDatastores = require('../../lib/datastores/memory');
-var testUtils = require('../test_utils');
-
-chai.should();
-var expect = chai.expect;
-
-describe('Instance (integration)', function() {
-
-  var date = 1264982400000;
-  var datetime = 1264982400000;
-
-  before(function() {
-    this.memoryHashStore = new memoryDatastores.MemoryHashStore();
-    testUtils.setupOrm.call(this);
-
-    this.Model = this.createModel({
-      scopes: {
-        all: [
-          'integer',
-          'string',
-          'booleanTrue',
-          'booleanFalse',
-          'date',
-          'datetime'
-        ]
-      },
-      attributes: {
-        integer: attributes.Integer({
-          hashStores: [this.memoryHashStore]
-        }),
-        string: attributes.String({
-          hashStores: [this.memoryHashStore]
-        }),
-        booleanTrue: attributes.Boolean({
-          hashStores: [this.memoryHashStore]
-        }),
-        booleanFalse: attributes.Boolean({
-          hashStores: [this.memoryHashStore]
-        }),
-        date: attributes.Date({
-          hashStores: [this.memoryHashStore]
-        }),
-        datetime: attributes.Datetime({
-          hashStores: [this.memoryHashStore]
-        })
-      }
-    });
-  });
-
-  beforeEach(function(done) {
-    this.memoryHashStore.reset(done);
-  });
-
-  beforeEach(function(done) {
-    // Save all different attribute types.
-    this.instance = this.Model.create({
-      integer: 123,
-      string: 'foo',
-      booleanTrue: true,
-      booleanFalse: false,
-      date: new Date(date),
-      datetime: new Date(datetime)
-    }, true);
-
-    this.instance.save(done);
-  });
-
-  describe('#save()', function() {
-
-    it('should create a new instance', function(done) {
-      testUtils.saveAndReload(this.instance, 'all', function(err, instance) {
-        if (err) {return done(err);}
-        instance.getId().should.exist;
-        instance.toObject('all').should.deep.eq({
-          integer: 123,
-          string: 'foo',
-          booleanTrue: true,
-          booleanFalse: false,
-          date: '2010-02-01',
-          datetime: '2010-02-01T00:00:00.000Z'
-        });
-        done();
-      });
-    });
-
-    it('should update an existing instance', function(done) {
-      var scope = ['integer'];
-      this.instance.set({integer: 456});
-      testUtils.saveAndReload(this.instance, scope, function(err, instance) {
-        if (err) {return done(err);}
-        instance.get('integer', true).should.eq(456);
-        done();
-      });
-    });
-
-    it('should set model status', function(done) {
-      this.instance.isNew.should.be.false;
-      this.instance.isChanged().should.be.false;
-      done();
-    });
-  });
-});
-
- */
