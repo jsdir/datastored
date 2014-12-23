8f2e7fa2fc8c8964adc22ca12e9de2bf2b8d2ee5
Jason Sommer <jsdir@users.noreply.github.com>
2014-12-22 23:47:16 -0600

    HasOne association

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var _ = require('lodash');
 
 var createOrm = require('./lib/orm');
-var relations = require('./lib/relations');
+var associations = require('./lib/associations');
 var attributes = require('./lib/attributes');
+var datastores = require('./lib/datastores');
 var utils = require('./lib/utils')
 
 module.exports = _.extend({
   createOrm: createOrm,
   mapAttributes: utils.mapAttributes
-}, relations, attributes);
+}, associations, attributes, datastores);
